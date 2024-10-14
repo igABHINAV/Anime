ce83de2a170e85a489ccf28501a02b8a68be8046
igAbhinav007 <tourparty3@gmail.com>
2024-10-15 00:00:12 +0530

    Update page.tsx
    Deathnote

--- src/app/deathnote/page.tsx
+++ src/app/deathnote/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      
+      DeathNote
+      <div classname="flex flex-col">
+       deathnote is the best anime i have ever watched in the history of my life ! 
+      </div>
     </div>
   )
 }
